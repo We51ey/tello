afb6b241a1ded1afb817c0ba62598a80d78695c0
Wesley <wl979159265@gmail.com>
2024-06-08 23:24:22 +0200

    modify structure

--- modules/loss.py
+++ modules/loss.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 def l2_loss(input, target, mask, batch_size):
     loss = (input - target) * mask
     loss = (loss * loss) / 2 / batch_size
 
     return loss.sum()
-=======
-def l2_loss(input, target, mask, batch_size):
-    loss = (input - target) * mask
-    loss = (loss * loss) / 2 / batch_size
-
-    return loss.sum()
->>>>>>> origin/master
